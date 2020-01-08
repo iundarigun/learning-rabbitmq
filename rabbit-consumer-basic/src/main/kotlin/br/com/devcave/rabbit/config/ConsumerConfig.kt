f7085f665f4660ee3b3c06166b68c2cfdc00f6b9
Oriol Canalias <oriol.canalias@wavy.global>
2020-01-08 15:44:17 -0300

    Reorganizing json Consumer

--- rabbit-consumer-basic/src/main/kotlin/br/com/devcave/rabbit/config/ConsumerConfig.kt
+++ rabbit-consumer-basic/src/main/kotlin/br/com/devcave/rabbit/config/ConsumerConfig.kt
@@ -1,32 +1,24 @@
 package br.com.devcave.rabbit.config
 
-import br.com.devcave.rabbit.domain.Person
 import br.com.devcave.rabbit.service.BasicListener
-import org.aopalliance.aop.Advice
 import org.slf4j.LoggerFactory
 import org.springframework.amqp.core.Message
 import org.springframework.amqp.core.Queue
 import org.springframework.amqp.core.QueueBuilder
 import org.springframework.amqp.rabbit.annotation.RabbitListener
-import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory
 import org.springframework.amqp.rabbit.connection.ConnectionFactory
 import org.springframework.amqp.rabbit.listener.MessageListenerContainer
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
-import org.springframework.amqp.support.converter.MessageConverter
-import org.springframework.beans.factory.support.DefaultListableBeanFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import javax.annotation.PostConstruct
 
 
 @Configuration
 class ConsumerConfig(
     private val connectionFactory: ConnectionFactory,
     private val basicListener: BasicListener,
-    private val simpleRabbitListenerContainerFactory: SimpleRabbitListenerContainerFactory,
-    private val messageConverter: MessageConverter
+    private val simpleRabbitListenerContainerFactory: SimpleRabbitListenerContainerFactory
 ) {
 
     private val log = LoggerFactory.getLogger(javaClass)
@@ -57,12 +49,4 @@ class ConsumerConfig(
         val bodyAsString = message.body?.let { String(it) } ?: ""
         log.info("body $bodyAsString")
     }
-
-    @RabbitListener(queues = ["JSON-QUEUE-BASIC"])
-    fun receiveMessageFromJsonQueue(message: Message) {
-        log.info("receive message from ${message.messageProperties.consumerQueue}")
-        val person = messageConverter.fromMessage(message)
-        log.info("body $person")
-    }
-
 }
---
+++ rabbit-consumer-basic/src/main/kotlin/br/com/devcave/rabbit/service/JsonConsumer.kt
@@ -0,0 +1,21 @@
+package br.com.devcave.rabbit.service
+
+import org.slf4j.LoggerFactory
+import org.springframework.amqp.core.Message
+import org.springframework.amqp.rabbit.annotation.RabbitListener
+import org.springframework.amqp.support.converter.MessageConverter
+import org.springframework.stereotype.Service
+
+@Service
+class JsonConsumer (
+    private val messageConverter: MessageConverter
+){
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    @RabbitListener(queues = ["JSON-QUEUE-BASIC"])
+    fun receiveMessageFromJsonQueue(message: Message) {
+        log.info("receive message from ${message.messageProperties.consumerQueue}")
+        val person = messageConverter.fromMessage(message)
+        log.info("body $person")
+    }
+}
